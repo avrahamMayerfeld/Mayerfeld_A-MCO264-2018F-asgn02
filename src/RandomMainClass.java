dc9e5ede5d60160241c76cee46d1a7f2fdc7c831
Your Name <you@example.com>
2018-10-22 04:08:01 -0400

    i started everything at once before i realized it has to be in three stages

---
+++ src/RandomMainClass.java
@@ -0,0 +1,84 @@
+
+public class RandomMainClass {
+/* one problem with this code is that if it is an Integer collection, adding 0 will be
+problematic, because in order not to increase the size upon adding, i test for 0 or "empty" or
+null and add to that index. I'm not sure of how to avoid that problem otherwise.
+	*/
+	
+	public static <E> RandomizedCollection randomC(int size, Object type) {
+		
+		if (type instanceof Integer) 
+		{
+		RandomizedCollection<Integer> R = new RandomizedCollection<Integer>(size);	
+			for (int i = 0; i < size; i++) { 
+				 R.initialFill(0);
+			}
+		return R;
+		}
+		
+		
+		else if (type instanceof String)
+		{
+			RandomizedCollection<String> R = new RandomizedCollection<>(size);
+		for (int i = 0; i < size; i++) {
+			  R.initialFill("EMPTY");
+			  }
+			  return R;
+		}
+		
+		else {
+			RandomizedCollection<Object> R = new RandomizedCollection<>(size);
+			for (int i = 0; i < size; i++) {
+				  R.initialFill(null);
+				}
+		return R;
+		}
+		
+	}
+	
+	
+	public static void main(String[] args) 
+	{
+	RandomizedCollection<Integer> RC1=randomC(5,"Integer");	
+	RC1.seeContents();
+	
+	RC1.add(12); 
+	 RC1.add(1); 
+	 RC1.add(1987); 
+	 RC1.add(387356); 
+	 RC1.add(3); 
+	 RC1.add(147); 
+	 RC1.seeContents();
+	 
+RC1.remove(0); 
+RC1.seeContents();
+	
+RC1.overwrite(0,13);
+RC1.seeContents();	 
+	 
+    System.out.println( RC1.getRandom() ); 
+System.out.println(RC1.getRandom(1,3));
+	 
+	
+	RandomizedCollection<String> RC2 = randomC(5,"String");
+	 RC2.seeContents();
+	
+	 RC2.add("one"); 
+	RC2.add("two");
+	RC2.add("three");
+	RC2.add("four");
+	RC2.add("five");
+	RC2.seeContents();
+	
+    RC2.remove(1); 
+    RC2.seeContents();
+    
+	RC2.overwrite(2,"seven");
+	 RC2.seeContents();
+	 
+    System.out.println(RC2.getRandom());
+    
+   System.out.println(RC2.getRandom(0,3));
+	    
+	}
+	}
---
+++ src/RandomizedCollection.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.util.stream.IntStream;
+public  class  RandomizedCollection<E>   {
+	
+	ArrayList<E> AL;
+	
+public RandomizedCollection(int size)
+	{
+	 AL=new ArrayList<>(size);
+		
+	}
+   
+
+   public void initialFill(E o)
+   {AL.add(o);}
+
+	public void add(E o) 
+	{
+     for(int i=0; i< AL.size();i++)
+     {
+		
+    	 if(AL.get(i).equals(0)||AL.get(i).equals("EMPTY")||AL.get(i).equals(null))
+    	 {AL.set(i,o);
+    	 break;
+    	 }
+     }
+     }
+	
+	
+	public void remove(int index)
+	{
+		AL.remove(index);
+	}
+	
+	public void overwrite(int index, E newElement) 
+	{
+		AL.set(index, newElement);
+	}
+	public void seeContents() 
+	{
+		for (int i=0; i< AL.size();i++)
+		{
+		System.out.println(AL.get(i).toString());
+		}
+	}
+	public E getRandom() {
+		Random r = new Random();
+		int rNum = r.nextInt(AL.size());
+		E randomItem;
+		randomItem = AL.get(rNum);
+		return randomItem;
+		}
+	public E getRandom(int start, int end) {
+		if(end> AL.size()|| start> AL.size())
+			try {
+				throw new Exception();
+			} catch (Exception e) {
+				
+				e.printStackTrace();
+			}
+		
+		Random r = new Random();
+		IntStream iS = r.ints(1, start, end+1);
+		int rNum = iS.findFirst().getAsInt();
+		E randomItem;
+		randomItem = AL.get(rNum);
+		return randomItem;
+		}
+
+	
+	
+	}
+
+
