733f0b8f47a9f86bf8cce286e900d17eab5d7b72
avrahamMayerfeld <44050391+avrahamMayerfeld@users.noreply.github.com>
2019-05-21 22:58:25 -0400

    fixed indentation

--- src/RandomMainClass.java
+++ src/RandomMainClass.java
@@ -9,28 +9,30 @@ null and add to that index. I'm not sure of how to avoid that problem otherwise.
 		
 		if (type instanceof Integer) 
 		{
-		RandomizedCollection<Integer> R = new RandomizedCollection<Integer>(size);	
-			for (int i = 0; i < size; i++) { 
+			RandomizedCollection<Integer> R = new RandomizedCollection<Integer>(size);	
+			for (int i = 0; i < size; i++) 
+			{ 
 				 R.initialFill(0);
 			}
 		return R;
 		}
-		
-		
 		else if (type instanceof String)
 		{
 			RandomizedCollection<String> R = new RandomizedCollection<>(size);
-		for (int i = 0; i < size; i++) {
-			  R.initialFill("EMPTY");
-			  }
-			  return R;
+			for (int i = 0; i < size; i++) 
+			{
+				R.initialFill("EMPTY");
+			}
+			return R;
 		}
 		
-		else {
+		else 
+		{
 			RandomizedCollection<Object> R = new RandomizedCollection<>(size);
-			for (int i = 0; i < size; i++) {
-				  R.initialFill(null);
-				}
+			for (int i = 0; i < size; i++)
+			{
+				R.initialFill(null);
+			}
 		return R;
 		}
 		
@@ -42,46 +44,46 @@ null and add to that index. I'm not sure of how to avoid that problem otherwise.
 		Integer intg = new Integer(6);
 		String str = "this string";
 		
-	RandomizedCollection<Integer> RC1=randomC(5,intg);	
-	RC1.seeContents();
+		RandomizedCollection<Integer> RC1=randomC(5,intg);	
+		RC1.seeContents();
 	
-	RC1.add(12); 
-	 RC1.add(1); 
-	 RC1.add(1987); 
-	 RC1.add(387356); 
-	 RC1.add(3); 
-	 RC1.add(147); 
-	 RC1.seeContents();
+		RC1.add(12); 
+	 	RC1.add(1); 
+		RC1.add(1987); 
+		RC1.add(387356); 
+		RC1.add(3); 
+	 	RC1.add(147); 
+		RC1.seeContents();
+
+		RC1.remove(0); 
+		RC1.seeContents();
+
+		RC1.overwrite(0,13);
+		RC1.seeContents();	 
+
+    		System.out.println( RC1.getRandom() ); 
+		System.out.println(RC1.getRandom(1,3));
 	 
-RC1.remove(0); 
-RC1.seeContents();
 	
-RC1.overwrite(0,13);
-RC1.seeContents();	 
-	 
-    System.out.println( RC1.getRandom() ); 
-System.out.println(RC1.getRandom(1,3));
-	 
+		RandomizedCollection<String> RC2 = randomC(5,str);
+	 	RC2.seeContents();
 	
-	RandomizedCollection<String> RC2 = randomC(5,str);
-	 RC2.seeContents();
+		RC2.add("one"); 
+		RC2.add("two");
+		RC2.add("three");
+		RC2.add("four");
+		RC2.add("five");
+		RC2.seeContents();
 	
-	 RC2.add("one"); 
-	RC2.add("two");
-	RC2.add("three");
-	RC2.add("four");
-	RC2.add("five");
-	RC2.seeContents();
-	
-    RC2.remove(1); 
-    RC2.seeContents();
+		RC2.remove(1); 
+		RC2.seeContents();
     
-	RC2.overwrite(2,"seven");
-	 RC2.seeContents();
+		RC2.overwrite(2,"seven");
+	 	RC2.seeContents();
 	 
-    System.out.println(RC2.getRandom());
+    		System.out.println(RC2.getRandom());
     
-   System.out.println(RC2.getRandom(0,3));
+   		System.out.println(RC2.getRandom(0,3));
 	    
 	}
-	}
+}
